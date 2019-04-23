06864aedfd57e8f857cbcaad8989f2d4f17d7fae
Lawren Juip <lawren.juip@detroitlabs.com>
2019-04-23 14:27:47 -0400

    Fixed the characterActivity to use the correct onCreate() method so that the fragment would be created. Have to fix a bug where the database access is occurring on main thread which causes a crash

--- app/src/main/java/com/example/fantasygenerator/activities/CharacterActivity.kt
+++ app/src/main/java/com/example/fantasygenerator/activities/CharacterActivity.kt
@@ -1,7 +1,6 @@
 package com.example.fantasygenerator.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fantasygenerator.R
 import com.example.fantasygenerator.fragments.CharacterDetailFragment
@@ -9,10 +8,10 @@ import com.example.fantasygenerator.models.Character
 import com.example.fantasygenerator.repo.AppDatabase
 import com.example.fantasygenerator.repo.CharacterRepository
 
-class MainActivity : AppCompatActivity() {
+class CharacterActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var character = Character()
